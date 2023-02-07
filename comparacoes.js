a025dd012946eb9283a916e06e480a2056db5492
Procopioo <124298444+Procopioo@users.noreply.github.com>
2023-02-07 17:28:28 -0300

    Add files via upload

---
+++ comparacoes.js
@@ -0,0 +1,27 @@
+// comparação implícita
+
+const numero = 5
+const texto = '5'
+console.log(numero == texto)
+
+// comparação explicita
+
+const Numero = 5
+const Texto  = '5'
+console.log(Numero === Texto)
+
+// type of
+console.log(typeof Numero)
+console.log(typeof Texto)
+
+// '==' só compara o valor
+// '!=' confere se o valor é diferente, caso for é retornado true.
+
+// '===' compara o valor e o tipo de dado guardado pela constante
+// '!==' confere se o valor e tipo de dado guardado é diferente, caso for é retornado true.
+
+
+// Operador de adição =	 x + y
+// Operador de subtração =	x - y
+// Operador de multiplicação =	x * y
+// Operador de divisão =  x / y
---
+++ ternario.js
@@ -0,0 +1,9 @@
+const idadeMínima = 18;
+const idadeCliente = 16;
+
+    if (idadeCliente >= idadeMínima){
+        console.log('cerveja')
+    }
+    else {
+        console.log('suco')
+    }
