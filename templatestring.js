9e5e4c2ee3b4e12fdb3768205da94e2cc33d65b5
Procopioo <124298444+Procopioo@users.noreply.github.com>
2023-02-07 17:28:01 -0300

    Add files via upload

---
+++ templatestring.js
@@ -0,0 +1,9 @@
+const nome = "Pedro"
+const idade = 2022 - 2005
+const cidadeDeNascimento = "Belo Horizonte"
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + ", nasci em " + cidadeDeNascimento
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
+
